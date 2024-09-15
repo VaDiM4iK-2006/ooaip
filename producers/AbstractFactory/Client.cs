0c16b8cc443a9e966c826641f30b7cd792562107
VaDiM4iK-2006 <vadimus77777@gmail.com>
2024-09-15 12:33:43 +0300

    upd

---
+++ producers/AbstractFactory/Client.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+    class Client
+    {
+        private IChair _chair;
+        private ITable _table;
+        private ISofa _sofa;
+
+        public Client(IAbstractFactory factory)
+        {
+            _chair = factory.CreateChair();
+            _table = factory.CreateTable();
+            _sofa = factory.CreateSofa();
+        }
+    }
+}
