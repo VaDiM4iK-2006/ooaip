085cc78946bf708b232fdf226a68773777bbdc05
VaDiM4iK-2006 <vadimus77777@gmail.com>
2024-09-15 12:16:53 +0300

    new

---
+++ producers/AbstractFactory/HTFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+    public class HTFactory : IAbstractFactory
+    {
+        public IChair CreateChair()
+        {
+            return new HTChair();
+        }
+        public ITable CreateTable()
+        {
+            return new HTTable();
+        }
+        public ISofa CreateSofa()
+        {
+            return new HTSofa();
+        }
+    }
+}
---
+++ producers/AbstractFactory/IAbstractFactory.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AbstractFactory
+{
+    public interface IAbstractFactory
+    {
+        public abstract IChair CreateChair();
+        public abstract ITable CreateTable();
+        public abstract ISofa CreateSofa();
+    }
+}
