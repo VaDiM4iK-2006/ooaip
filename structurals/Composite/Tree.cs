e76869677cf5bba1f38716143dffaabe5ee6ac81
VaDiM4iK-2006 <vadimus77777@gmail.com>
2024-11-29 19:01:03 +0200

    fix3

--- structurals/Composite/Tree.cs
+++ structurals/Composite/Tree.cs
@@ -8,21 +8,19 @@ namespace Composite
         class Node
         {
             public IComponent Value { get; set; }
-            public Node Left { get; set; }
-            public Node Right { get; set; }
+            public Node? Left { get; set; }
+            public Node? Right { get; set; }
             public Node(IComponent val)
             {
                 Value = val;
                 Left = Right = null;
             }
         }
+
         Node root = null;
-        int count;
-        internal int Count
-        {
-            get { return count; }
-            private set { count = value; } 
-        }
+
+        internal int count { get; private set; }
+
         public void Add(IComponent val)
         {
             count++;
@@ -34,8 +32,8 @@ namespace Composite
             Node current = root;
             while (true)
             {
-                double currValue = current.Value.GetValue().First();
-                double newValue = val.GetValue().First();
+                var currValue = current.Value.GetValue().First();
+                var newValue = val.GetValue().First();
 
                 if (currValue < newValue)
                 {
@@ -45,7 +43,9 @@ namespace Composite
                         return;
                     }
                     else
+                    {
                         current = current.Left;
+                    }
                 }
                 else
                 {
@@ -55,23 +55,25 @@ namespace Composite
                         return;
                     }
                     else
+                    {
                         current = current.Right;
+                    }
                 }
             }
         }
 
-        private IEnumerable<IComponent> InOrderWalk(Node t)
+        private IEnumerable<IComponent> LRR(Node t)
         {
             if (t != null)
             {
-                foreach (var left in InOrderWalk(t.Left))
+                foreach (var left in LRR(t.Left))
                 {
                     yield return left;
                 }
 
                 yield return t.Value;
 
-                foreach (var right in InOrderWalk(t.Right))
+                foreach (var right in LRR(t.Right))
                 {
                     yield return right;
                 }
@@ -80,8 +82,9 @@ namespace Composite
 
         public IEnumerator<IComponent> GetEnumerator()
         {
-            return InOrderWalk(root).GetEnumerator();
+            return LRR(root).GetEnumerator();
         }
+
         IEnumerator IEnumerable.GetEnumerator()
         {
             return GetEnumerator();
