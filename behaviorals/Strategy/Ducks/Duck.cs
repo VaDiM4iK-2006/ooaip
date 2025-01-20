933f697944aff1aaa6dbdf6a240abc4df1469908
VaDiM4iK-2006 <vadimus77777@gmail.com>
2025-01-20 23:37:25 +0200

    stra

--- behaviorals/Strategy/Ducks/Duck.cs
+++ behaviorals/Strategy/Ducks/Duck.cs
@@ -1,7 +1,35 @@
-﻿namespace Strategy.Ducks
+﻿using Strategy.Fly;
+using Strategy.Quack;
+using Strategy.Swim;
+
+namespace Strategy.Ducks
 {
     public abstract class Duck
     {
+        protected IFly _fly { get; }
+        protected ISwim _swim { get; }
+        protected IQuack _quack { get; }
 
+        public Duck(IFly fly, ISwim swim, IQuack quack)
+        {
+            _fly = fly == null ? throw new ArgumentNullException() : fly;
+            _swim = swim == null ? throw new ArgumentNullException() : swim;
+            _quack = quack == null ? throw new ArgumentNullException() : quack;
+        }
+
+        public string DuckFly()
+        {
+            return _fly.Fly();
+        }
+
+        public string DuckSwim()
+        {
+            return _swim.Swim();
+        }
+
+        public string DuckQuack()
+        {
+            return _quack.Quack();
+        }
     }
 }
---
+++ behaviorals/Strategy/Fly/NoFly.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy.Fly
+{
+    public class NoFly : IFly
+    {
+        public string Fly()
+        {
+            return "duck is not able to fly";
+        }
+    }
+}
